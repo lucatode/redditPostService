6b5d79653ff66e378e0c8c232b2bc809ac103af0
unknown <l.todeschini88@gmail.com>
2018-07-15 03:11:28 +0200

    starting new service

---
+++ src/main/java/com/lucatode/redditPostService/RedditPostServiceApplication.java
@@ -0,0 +1,12 @@
+package com.lucatode.redditPostService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditPostServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedditPostServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/lucatode/redditPostService/RedditPostServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lucatode.redditPostService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RedditPostServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
